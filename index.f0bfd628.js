4a82b70b2a1c5bf5ddd16042ae77dc013956eb71
d1mkn <46346986+d1mkn@users.noreply.github.com>
2023-03-03 02:26:19 +0000

    Deploying to gh-pages from @ d1mkn/weather-app@5d09a23ab8dd58844a69ce2880b748d9aada559d 🚀

--- index.f0bfd628.js
+++
@@ -1,2 +0,0 @@
-async function fetchData(){return await fetch("https://api.openweathermap.org/data/2.5/weather?q=Uman&appid=798214fee8cf5a1c98981c6f53aa8c25")}fetchData().then((a=>a.json())).then((a=>console.log(a.hits)));
-//# sourceMappingURL=index.f0bfd628.js.map
