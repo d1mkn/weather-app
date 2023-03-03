47869570f7592893474cdca8384d3941129d639b
Dmytro Hubin <dimkanishe2099@gmail.com>
2023-03-03 16:58:55 +0200

    add-geo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import fetchWeatherData from './weather-api';
+import { fetchByName, fetchByCords } from './weather-api';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   form: document.querySelector('#api_form'),
   submitBtn: document.querySelector('[type=submit]'),
+  aboutSec: document.querySelector('.about'),
   cityEl: document.querySelector('#yourCity'),
   weatherEl: document.querySelector('.current-weather'),
   tempretureEl: document.querySelector('.current-temperature'),
@@ -11,23 +13,47 @@ const refs = {
 };
 
 refs.form.addEventListener('submit', onSubmit);
+window.addEventListener('load', function () {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(findGeo);
+  } else {
+    x.innerHTML = 'Geolocation is not supported by this browser.';
+  }
+});
+
+async function findGeo(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  //fetch
+  const weatherFetch = await fetchByCords(lat, lon);
+  const weatherData = weatherFetch.data;
+  // to local storage
+  localStorage.setItem('city', weatherData.name);
+  // render
+  await renderMarkup(weatherData);
+}
 
 async function onSubmit(e) {
   e.preventDefault();
 
-  const query = e.currentTarget.query.value;
-
-  // fetch
-  const weatherFetch = await fetchWeatherData(query);
-  const weatherData = weatherFetch.data;
-  console.log(weatherData);
-
-  localStorage.setItem('city', weatherData.name);
-
-  // change city
-  refs.cityEl.innerHTML = weatherData.name;
-
-  renderMarkup(weatherData);
+  try {
+    const query = e.currentTarget.query.value.trim();
+    if (query === '') {
+      return Notify.info('Enter the city you are looking for.');
+    }
+    // fetch
+    const weatherFetch = await fetchByName(query);
+    const weatherData = weatherFetch.data;
+    // to local storage
+    localStorage.setItem('city', weatherData.name);
+    // render
+    await renderMarkup(weatherData);
+  } catch (error) {
+    console.log(error);
+    return Notify.failure(
+      'Weather forecast for this city is currently unavailable.'
+    );
+  }
 }
 
 async function renderMarkup(weatherData) {
@@ -43,7 +69,7 @@ async function renderMarkup(weatherData) {
 
 async function oldQueryMarkup() {
   if (localStorage.city) {
-    const weatherFetch = await fetchWeatherData(localStorage.city);
+    const weatherFetch = await fetchByName(localStorage.city);
     const weatherData = weatherFetch.data;
     renderMarkup(weatherData);
   }
--- src/weather-api.js
+++ src/weather-api.js
@@ -5,9 +5,18 @@ const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 axios.defaults.baseURL = BASE_URL;
 
-export default async function fetchWeatherData(query) {
+async function fetchByName(query) {
   const response = await axios.get(
     `?q=${query}&appid=${API_KEY}&wind=wind.speed.unit&lastupdate=lastupdate.value&units=metric`
   );
   return response;
 }
+
+async function fetchByCords(latitude, longitude) {
+  const response = await axios.get(
+    `?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&wind=wind.speed.unit&lastupdate=lastupdate.value&units=metric`
+  );
+  return response;
+}
+
+export { fetchByName, fetchByCords };
